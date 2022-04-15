f07fba5e7f6d214bcb90279d63a28731488de7bc
webworld420 <76555387+webworld420@users.noreply.github.com>
2022-04-15 10:36:14 +0600

    Add files via upload

---
+++ Custom.js
@@ -0,0 +1,216 @@
+$(".btn-success").click(function(){
+   alert("Please Add Your Banking Card")
+    $('.Shopping-page').hide(200);
+    $('')
+})
+
+
+
+
+$('.cross i').click(function(){
+    $('.Bank-Card-area').hide(200);
+  })
+     $('.cardNumber .submit').click(function(){
+    $('.Bank-Card-area').hide(500);
+    alert("Thanks for your Card Number,  Happy Shopping")
+
+  })
+
+
+  
+  $(".card .btn-danger1").click(function(){
+    $('.Shopping-page .card1').hide(100);
+  });
+  $(".card .btn-danger2").click(function(){
+    $('.Shopping-page .card2').hide(100);
+  });
+  $(".card .btn-danger3").click(function(){
+    $('.Shopping-page .card3').hide(100);
+  });
+  $(".card .btn-danger4").click(function(){
+    $('.Shopping-page .card4').hide(100);
+  });
+
+
+
+    $(".ddCard").click(function(){
+    $('section').hide();
+    $(".Shopping-page").show(300);
+     });
+      $(".listed-product").click(function(){
+    $('section').toggle();
+    $(".Shopping-page").show(300);
+    $('a').click(function(){
+      $('section').show()
+    });
+     });
+     
+     $(".addBank").click(function(){
+    $(".Bank-Card-area").show(300);
+     });
+
+
+
+
+
+     document.getElementById('addCard').addEventListener("click", function(){
+
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne = 01;
+      const AdditionProduct = addOne + ConvertBeforeProduct;
+     document.getElementById('ProductCount').innerText = "0" + AdditionProduct;
+
+     })
+
+   document.getElementById('addCard2').addEventListener("click", function(){
+
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne = 01;
+      const AdditionProduct = addOne + ConvertBeforeProduct;
+     document.getElementById('ProductCount').innerText = "0" + AdditionProduct;
+
+     })
+
+   document.getElementById('addCard3').addEventListener("click", function(){
+
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne = 01;
+      const AdditionProduct = addOne + ConvertBeforeProduct;
+     document.getElementById('ProductCount').innerText = "0" + AdditionProduct;
+
+     })
+
+   document.getElementById('addCard4').addEventListener("click", function(){
+
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne = 01;
+      const AdditionProduct = addOne + ConvertBeforeProduct;
+     document.getElementById('ProductCount').innerText = "0" + AdditionProduct;
+
+     })
+
+   document.getElementById('addCard5').addEventListener("click", function(){
+
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne = 01;
+      const AdditionProduct = addOne + ConvertBeforeProduct;
+     document.getElementById('ProductCount').innerText = "0" + AdditionProduct;
+
+     })
+
+   document.getElementById('addCard6').addEventListener("click", function(){
+
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne = 01;
+      const AdditionProduct = addOne + ConvertBeforeProduct;
+     document.getElementById('ProductCount').innerText = "0" + AdditionProduct;
+
+     })
+
+   document.getElementById('addCard7').addEventListener("click", function(){
+
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne = 01;
+      const AdditionProduct = addOne + ConvertBeforeProduct;
+     document.getElementById('ProductCount').innerText = "0" + AdditionProduct;
+
+     })
+
+
+ document.getElementById('addCard9').addEventListener("click", function(){
+
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne = 01;
+      const AdditionProduct = addOne + ConvertBeforeProduct;
+     document.getElementById('ProductCount').innerText = "0" + AdditionProduct;
+
+     })
+
+   
+
+
+
+     
+     ////////
+
+     document.getElementById('danger4').addEventListener("click", function(){
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne = 1;
+      const AdditionProduct = ConvertBeforeProduct - addOne;
+     document.getElementById('ProductCount').innerText =  AdditionProduct;
+
+     })
+
+     document.getElementById('danger3').addEventListener("click", function(){
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne =1;
+      const AdditionProduct =ConvertBeforeProduct - addOne;
+     document.getElementById('ProductCount').innerText =  AdditionProduct;
+
+     })
+ document.getElementById('danger2').addEventListener("click", function(){
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne = 1;
+      const AdditionProduct = ConvertBeforeProduct - addOne;
+     document.getElementById('ProductCount').innerText =  AdditionProduct;
+
+     })
+
+ document.getElementById('danger1').addEventListener("click", function(){
+      
+      const BeforeProduct = document.getElementById('ProductCount').innerText;
+      const ConvertBeforeProduct=parseFloat(BeforeProduct);
+      console.log(ConvertBeforeProduct)
+
+      const addOne = 1;
+      const AdditionProduct = ConvertBeforeProduct - addOne;
+     document.getElementById('ProductCount').innerText =  AdditionProduct;
+
+     })
+
+   
+     
